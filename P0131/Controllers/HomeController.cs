1e1759c39543b1840e10df58ef593a3b9bfef35a
Eduard Bumbu <reduardbumbu@gmail.com>
2022-02-01 13:43:38 +0200

    Add project files.

---
+++ P0131/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using P0131.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace P0131.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            string hour = DateTime.Now.ToString();
+            ViewBag.Message = hour;
+         
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ P0131/Models/ShowTime.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace P0131
+{
+    public class ShowTime
+    {
+       
+        public ShowTime()
+        {
+
+        }
+        public DateTime GetCurrentTime()
+        {
+            DateTime dt = DateTime.Now.ToLocalTime();
+            return dt;
+        }
+
+        
+    }
+}
